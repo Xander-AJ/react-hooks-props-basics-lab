23e54f0c3d0663e59ee3909391c9188831d9e84e
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-04-24 18:01:28 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links"; // Import Links component
+import image from "../data/data"; // Import image URL
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {bio && bio.trim() !== "" && <p>{bio}</p>} {/* Conditional rendering for bio */}
+      <img src={image} alt="I made this" />
+      <Links github={github} linkedin={linkedin} /> {/* Pass github and linkedin props to Links */}
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from "../data/user"; // Import user data
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* Pass name and city as props to Home component */}
+      <Home name={user.name} city={user.city} />
+      {/* Pass bio, github, and linkedin as props to About component */}
+      <About
+        bio={user.bio}
+        github={user.github}
+        linkedin={user.linkedin}
+      />
     </div>
   );
 }
